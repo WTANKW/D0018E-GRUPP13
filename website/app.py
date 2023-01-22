ecede3b9b7607f6b9b62a8f7e676468f1ee8a5f6
Linus Håkegård <linhke-0@student.ltu.se>
2023-01-22 14:01:46 +0000

    added database

--- website/app.py
+++ website/app.py
@@ -1,12 +1,25 @@
 from flask import Flask, render_template
+from flask_mysqldb import MySQL
 #source env/bin/activate
 #flask run
 
 app = Flask(__name__)
 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config["MYSQL_USER"] = 'root'
+app.config["MYSQL_PASSWORD"] = '123456789'
+app.config["MYSQL_DB"] = 'test'
+
+mysql = MySQL(app)
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM Frog")
+    fetchdata = cur.fetchall()
+    cur.close()
+ 
+    return render_template('index.html', data = fetchdata)
 
 @app.route('/other_page')
 def annan_funktion():
