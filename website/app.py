f86d78c044ea2778630794f78fb647322fe0bb55
Linus Håkegård <linhke-0@student.ltu.se>
2023-01-27 08:56:41 +0000

    login basics

--- website/app.py
+++ website/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from flask_mysqldb import MySQL
+from flask_login import UserMixin
 #source env/bin/activate
 #flask run
 
@@ -15,7 +16,7 @@ mysql = MySQL(app)
 @app.route('/')
 def index():
     cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM Frog")
+    cur.execute("SELECT * FROM User")
     fetchdata = cur.fetchall()
     cur.close()
  
@@ -39,7 +40,7 @@ def signup():
         cur.execute("SELECT EXISTS(SELECT * FROM User WHERE email = %s)", (email,))
         emailInUse = cur.fetchall()
    
-        if emailInUse[0][0]:
+        if emailInUse[0][0] or password == "":
             cur.close()
             return render_template('signup.html', data = "Signup Failed, Please try again")
 
@@ -48,6 +49,27 @@ def signup():
         cur.close()
 
         return redirect("/", code=302)
+
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    if request.method == "GET":
+        return render_template('login.html')
+
+    if request.method == "POST":
+        #print("test")
+        email = request.form['email']
+        password = request.form['password']
+        
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT EXISTS(SELECT * FROM User WHERE email = %s AND password = %s)", (email, password))
+        loginStatus = cur.fetchall()
+        print(loginStatus[0][0])
+        cur.close()
+
+        if loginStatus[0][0]:
+            return redirect('/')
+
+        return redirect("/login", code=302)
 
 if __name__ == "__main__":
     app.run(debug = True)
